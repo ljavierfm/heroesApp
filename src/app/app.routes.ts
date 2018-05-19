78ed02fa13979f2227105b3a4573c04a755123d3
Javier Fernandez <ljavierfm@gmail.com>
2018-05-19 12:59:41 +0200

    Listado de heroes y formulario de agregar/editar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroeComponent } from './components/heroes/heroe.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 const routes: Routes = [
-    { path: 'heroes', component: HeroeComponent },
+    { path: 'heroes', component: HeroesComponent },
     { path: 'heroe/:id', component: HeroeComponent },
     { path: '**', pathMatch:'full',redirectTo:'heroes' }
 ];
