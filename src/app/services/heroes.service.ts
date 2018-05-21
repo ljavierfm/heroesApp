97ade631400592f19b71e06716554cd2d4e730d7
Javier Fernandez <ljavierfm@gmail.com>
2018-05-21 20:06:10 +0200

    Se crea archivo de configuracion

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Headers,Http,Response } from '@angular/http';
 import { Heroe } from '../interfaces/heroe.interface';
+import { cadenasConexion } from '../config';
+
 import 'rxjs/Rx';
 
 @Injectable()
 export class HeroesService {
-
-
   constructor(private _http: Http) { }
+
+  heroesUrl=cadenasConexion.heroesUrl;
+  heroeUrl=cadenasConexion.heroeUrl;
 
   nuevoHeroe(heroe:Heroe){
     let body=JSON.stringify(heroe);
